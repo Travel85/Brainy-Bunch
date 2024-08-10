68f319442a0608187f458b6a82fb1c85f898e5ea
Travel85 <31290733+Travel85@users.noreply.github.com>
2024-08-10 11:12:42 +0200

    Updated Readme

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -6,6 +6,7 @@ import "@fontsource/roboto/700.css";
 //MUI imports
 import Container from "@mui/material/Container";
 import Box from "@mui/material/Box";
+import Alert from "@mui/material/Alert";
 
 //REACT imports
 import { Fragment } from "react";
@@ -13,8 +14,11 @@ import { Fragment } from "react";
 function App() {
   return (
     <Container>
-      <h1>Brainy Bunch!</h1>
-      <Box></Box>
+      <h1 align="center">Brainy Bunch!</h1>
+      <h2 align="center">Where Brilliance Blooms!</h2>
+      <Box>
+        <Alert severity="success">Brainy bunch rocks!</Alert>
+      </Box>
     </Container>
   );
 }
